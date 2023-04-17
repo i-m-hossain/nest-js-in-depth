8ee6070c64b15ad1117a877893524cf65b778270
imranhAltersense <114974565+imranhAltersense@users.noreply.github.com>
2023-04-17 15:45:07 +0600

    typeorm added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { CustomerModule } from './customers/customer.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CustomerModule, UsersModule],
+  imports: [
+    CustomerModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'testuser',
+      password: 'testuser',
+      database: 'nestjs_in_depth',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
